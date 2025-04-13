0a8c620ca2b8599a4a3add9f89100470f4613469
WangilaPrish <nabwonyaprish@gmail.com>
2025-04-13 20:55:09 +0300

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,11 +5,8 @@ import { updateCartQuantity } from '../amazon.js';
 import{ hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import  dayjs  from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary} from './paymentSummary.js'
 
-hello();
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd,  MMMM D'));
 
 export function renderOrderSummary(){
 
@@ -118,7 +115,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     removeFromCart(productId);
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
     container.remove();
-    updateCartQuantity();
+    renderPaymentSummary(); 
     
   });
 });
@@ -162,6 +159,7 @@ document.querySelectorAll('.js-delivery-option').forEach((element) => {
     const {productId, deliveryOptionId} = element.dataset;
     updateDeliveryOption(productId, deliveryOptionId);
     renderOrderSummary();
+    renderPaymentSummary();
   });
 });
 }
